a172e0753c9bb92fe23666d7cd7d6c9358fb91bc
Reed Klaeser <reed.klaeser@gmail.com>
2025-02-16 14:48:42 -0800

    react edit

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
 import Counter from './Components/Counter'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
      <Counter />
